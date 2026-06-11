c46c61f2a4299da2da93755c4b5330b2c8e72d76
FADM ECNM CORY ANDREW HOFSTAD USN <96269532+FADM-DCMN-CORY-A-HOFSTAD-USN@users.noreply.github.com>
2026-06-11 14:16:23 -0700

    Create cuda_check.h

---
+++ include/cuda_check.h
@@ -0,0 +1,23 @@
+pragma once
+include <iostream>
+
+if defined(HAS_CUDA_SUPPORT)
+    include <cuda_runtime.h>
+endif
+
+inline bool is_nvidia_hardware_available() {
+    if !defined(HAS_CUDA_SUPPORT) return false;
+
+    if defined(HAS_CUDA_SUPPORT)
+        int device_count = 0;
+        cudaError_t error = cudaGetDeviceCount(&device_count);
+        
+        if (error != cudaSuccess) return false;
+        if (device_count <= 0) return false;
+
+        cudaDeviceProp device_prop;
+        cudaGetDeviceProperties(&device_prop, 0);
+        std::cout << "[INFO] NVIDIA GPU Detected: " << device_prop.name << "\n";
+        return true;
+    endif
+}
